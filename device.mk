565f182a28fd637dea23a58ea522c6b9aa16c7bc
dianlujitao <dianlujitao@lineageos.org>
2018-02-20 22:47:08 +0800

    rolex: Build LiveDisplay HAL
    Change-Id: I4bfac85c64599ced45983a86d98cfd810a97db99

--- device.mk
+++ device.mk
@@ -217,6 +217,10 @@ PRODUCT_PACKAGES += \
     android.hardware.light@2.0-impl \
     android.hardware.light@2.0-service
 
+# LiveDisplay native
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay@1.0-service-sdm
+
 # Media
 PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_audio.xml \
