b151aacf593dff500924bf05c1f4f5f3accf3395
Bruno Martins <bgcngm@gmail.com>
2019-01-21 09:40:51 +0000

    rolex: Migrate to livedisplay 2.0
    Change-Id: I513c284c534ce3abc27bf374513280171454f4d1

--- device.mk
+++ device.mk
@@ -123,7 +123,6 @@ PRODUCT_PACKAGES += \
     hwcomposer.msm8937 \
     libdisplayconfig \
     libqdMetaData.system \
-    libjni_livedisplay \
     libtinyxml \
     memtrack.msm8937
 
@@ -217,9 +216,9 @@ PRODUCT_PACKAGES += \
     android.hardware.light@2.0-impl \
     android.hardware.light@2.0-service
 
-# LiveDisplay native
+# LiveDisplay
 PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@1.0-service-sdm
+    vendor.lineage.livedisplay@2.0-service-sdm
 
 # Media
 PRODUCT_COPY_FILES += \
