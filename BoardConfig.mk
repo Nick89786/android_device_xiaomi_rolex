167a49bb92fb9e57fa8fb0de4465fb14f3abe2ac
Nick <nikhilnick00007@gmail.com>
2020-02-22 17:27:48 +0000

    rolex: update vendor security patch

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -177,7 +177,7 @@ TARGET_PROVIDES_QTI_TELEPHONY_JAR := true
 TARGET_USES_OLD_MNC_FORMAT := true
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2019-06-01
+VENDOR_SECURITY_PATCH := 2019-11-01
 
 # SELinux
 #include device/qcom/sepolicy-legacy-um/sepolicy.mk
